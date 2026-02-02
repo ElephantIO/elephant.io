413db1d53c59791ef47d1c98c671f5fb03ada413
Toha <tohenk@yahoo.com>
2026-02-02 07:48:53 +0700

    Update file header.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the Elephant.io package
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ * @copyright ElephantIO
+ * @copyright Wisembly
+ * @license   http://www.opensource.org/licenses/MIT-License MIT License
+ */
+
 error_reporting(E_ALL);
 set_error_handler('error_to_exception');
 
